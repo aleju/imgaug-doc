1de690fb5534b7980578b647e55a1cff61a3d50f
aleju <kontakt@ajung.name>
2020-01-18 18:22:24 +0100

    Fix temp folder not being deleted

--- scripts/generate_rtd_images/gen_overview_debug.py
+++ scripts/generate_rtd_images/gen_overview_debug.py
@@ -22,28 +22,28 @@ def chapter_augmenters_savedebugimageeverynbatches():
     bbsoi = ia.quokka_bounding_boxes((128, 128), extract="square")
     segmaps = ia.quokka_segmentation_map((128, 128), extract="square")
 
-    folder_path = tempfile.mkdtemp()
-    seq = iaa.Sequential([
-        iaa.Sequential([
-            iaa.Fliplr(0.5),
-            iaa.Crop(px=(0, 16))
-        ], random_order=True),
-        iaa.SaveDebugImageEveryNBatches(folder_path, 100)
-    ])
+    with tempfile.TemporaryDirectory() as folder_path:
+        seq = iaa.Sequential([
+            iaa.Sequential([
+                iaa.Fliplr(0.5),
+                iaa.Crop(px=(0, 16))
+            ], random_order=True),
+            iaa.SaveDebugImageEveryNBatches(folder_path, 100)
+        ])
 
-    _ = seq(images=[image] * 4,
-            segmentation_maps=[segmaps] * 4,
-            bounding_boxes=[bbsoi] * 4)
+        _ = seq(images=[image] * 4,
+                segmentation_maps=[segmaps] * 4,
+                bounding_boxes=[bbsoi] * 4)
 
-    image_debug_path = os.path.join(folder_path, "batch_latest.png")
-    image_debug = imageio.imread(image_debug_path)
+        image_debug_path = os.path.join(folder_path, "batch_latest.png")
+        image_debug = imageio.imread(image_debug_path)
 
-    utils.save(
-        "overview_of_augmenters",
-        fn_start + ".jpg",
-        image_debug,
-        quality=95
-    )
+        utils.save(
+            "overview_of_augmenters",
+            fn_start + ".jpg",
+            image_debug,
+            quality=95
+        )
 
 
 if __name__ == "__main__":
